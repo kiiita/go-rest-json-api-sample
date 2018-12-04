db0c7452ad70aebf8ecabd03915097f1c34a2f68
Yuto Kitakuni <yuto.kitakuni@gmail.com>
2018-12-04 17:09:07 +0900

    refactor: formatting

--- todo.go
+++ todo.go
@@ -3,10 +3,10 @@ package main
 import "time"
 
 type Todo struct {
-  ID        int    `json:"id"`
-  Name      string `json:"name"`
-  Completed bool   `json:"complted"`
-  Due time.Time    `json:"due"`
+	ID        int       `json:"id"`
+	Name      string    `json:"name"`
+	Completed bool      `json:"complted"`
+	Due       time.Time `json:"due"`
 }
 
 type Todos []Todo
