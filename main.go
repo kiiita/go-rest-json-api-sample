ede55e22a232d267fc9d90219eb66918efb748d5
Yuto Kitakuni <yuto.kitakuni@gmail.com>
2018-12-04 17:08:46 +0900

    feat(todo): Create POST endpoint

--- main.go
+++ main.go
@@ -1,17 +1,18 @@
 package main
 
 import (
-  "log"
-  "net/http"
+	"log"
+	"net/http"
 
-  "github.com/julienschmidt/httprouter"
+	"github.com/julienschmidt/httprouter"
 )
 
 func main() {
-  router := httprouter.New()
-  router.GET("/", Logging(Index, "index"))
-  router.GET("/todos", Logging(TodoIndex, "todo-index"))
-  router.GET("/todos/:todoId", Logging(TodoShow, "todo-show"))
+	router := httprouter.New()
+	router.GET("/", Logging(Index, "index"))
+	router.GET("/todos", Logging(TodoIndex, "todo-index"))
+	router.GET("/todos/:todoId", Logging(TodoShow, "todo-show"))
+	router.POST("/todos", Logging(TodoCreate, "todo-create"))
 
-  log.Fatal(http.ListenAndServe(":8080", router))
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
